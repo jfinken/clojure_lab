1bf6d139185e72ef6dc412137e2a97f266c88b94
Josh Finken <josh@earthmine.com>
2010-10-04 19:22:47 -0700

    - committing a working version of pig-latinize

--- proj/dialect/src/dialect.clj
+++ proj/dialect/src/dialect.clj
@@ -1,8 +1,21 @@
 (ns dialect
-  (:require [clojure.contrib.string :as str]))
+  (:require [clojure.contrib.string :as s]))
 (defn canadianize [sentence] (str sentence ", eh"))
-;"ig-Pay atinize-Lay s-Iay oming-Cay oon-Say!"
+
+; playing with type hinted string - unused here
+(defn str-len [^String x]
+  (.length x))
+
+(defn pig-latin [word]
+  ;take first letter, capitalize, add to end with '-ay'
+  (str 
+    (s/tail (- (.length word) 1) word)
+    "-"
+    (s/upper-case (str (get word 0)))
+    "ay "))
+  
 (defn pig-latinize [sentence] 
-  (str/map-str 
-    str
-    (str/partition #" " sentence)))
+  (s/chop
+    (s/map-str 
+      pig-latin    
+      (.split sentence " "))))
