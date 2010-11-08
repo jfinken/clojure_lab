45770625ade58313d416d0acf1a04c76fca782a9
Josh Finken <josh@earthmine.com>
2010-11-07 19:21:38 -0800

    - more work on the update-model lab.

--- proj/update-model/project.clj
+++ proj/update-model/project.clj
@@ -1,4 +1,6 @@
 (defproject update-model "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                   [org.clojure/clojure-contrib "1.2.0"]])
+                   [org.clojure/clojure-contrib "1.2.0"]
+                   [ring/ring-core "0.3.3"]
+                   [http.async.client "0.2.1"]])
--- proj/update-model/src/update_model/core.clj
+++ proj/update-model/src/update_model/core.clj
@@ -1,7 +1,10 @@
 (ns update-model.core
-  (:refer-clojure :exclude [get]))
+  (:refer-clojure :exclude [get])
+  (:use [ring.util.codec :as codec])
+  (:use [clojure.contrib.json :only (read-json)])
+  (:require [http.async.client :as http]))
 
-; Use atoms to build a simple object cache. 
+  ; Use atoms to build a simple object cache. 
 ; First, create a create-1 function that returns an atom wrapped around an empty map.
 (defn create-1
   []
@@ -31,3 +34,52 @@
 (let [c (create {:josh 34})] 
   (put c :bryce 29)
   (put c {:oliver 16, :tom 31}))
+
+;-----------------------------------------------------------------------------
+; Refs
+;-----------------------------------------------------------------------------
+(defn create
+  "cache based on refs"
+  ([] (create {}))
+  ([m] (ref m)))
+
+(defn get-r
+  [cache key]
+  (@cache key))
+
+(defn put
+  ([cache key val]
+    (dosync (alter cache assoc key val)))
+  ([cache entire-set]
+    (dosync (alter cache merge entire-set))))
+      
+(defn fast-put
+  ([cache key val]
+    (dosync (commute cache assoc key val)))
+  ([cache entire-set]
+    (dosync (commute cache merge entire-set))))
+
+(defn put-josh
+  ([cache key val] 
+    (dosync (ref-set cache (assoc @cache key val))))
+  ([cache entire-map] 
+    (dosync (ref-set cache (conj @cache entire-map)))))
+
+;-----------------------------------------------------------------------------
+; Futures
+;-----------------------------------------------------------------------------
+(def google-search-base
+  "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=")
+
+(defn gsearch
+  [terms]
+  (let [response (http/GET (str google-search-base (codec/url-encode terms)))]
+        (http/await response)
+        (http/string response)))
+
+(defn est-hit-count
+  [terms]
+  (java.lang.Long/parseLong
+    (:estimatedResultCount 
+      (:cursor 
+        (:responseData (read-json (gsearch terms)))))))
