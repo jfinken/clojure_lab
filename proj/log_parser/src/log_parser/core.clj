78f315043f25a31736c3f76a7aa45e2ad65e7f3d
Josh Finken <josh@earthmine.com>
2012-03-02 16:46:11 -0800

    - changes to the earthmine server log parser.  Make maps of the api call response times and merge them down with an average function.

--- proj/log_parser/src/log_parser/core.clj
+++ proj/log_parser/src/log_parser/core.clj
@@ -77,17 +77,69 @@
 
 
 (def temp "INFO [com.bitgirder.servlet.ServiceServlet]: 82.216.88.111, Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1) ; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729), zgc6ypp4bz5drttcc524phnq [Sun, 27 Mar 2011 17:37:32 +0000] get-plane-pano, 73 ms")
-;------------------------------------------------------------------------------
-; this will parse out the duration of the api call from the line. 
-; TO-DO: generate a map with averaged times, i.e {"get-panoramas" 13}
-;------------------------------------------------------------------------------
-(assoc {}
-  (get-res (subs temp (first (keys (re-pos #"\bget-\b" temp))))
-    #"\bget-[A-Za-z]+-*[A-Za-z]*-*[A-Za-z]*\b")
-  (Integer/parseInt
-    (get-res (subs temp (first (keys (re-pos #"\bget-\b" temp))))
-         #"\b[0-9]+\b")))
 
+;------------------------------------------------------------------------------
+; API call response time processing
+;
+; this will parse out the duration of the api call from the line. 
+; i.e {"get-panoramas" 13}
+; TO-DO: generate a map with averaged times
+;------------------------------------------------------------------------------
+(defn get-api-call-time
+  "Given a line from the server log (string), return a map of the
+  call and it response processing time in ms."
+  [line]
+    (let [call (first (keys (re-pos #"\bget-\b" line)))]
+    ; could be nil if the regexp is not found
+    (if (nil? call)
+      {}
+      (assoc {}
+        ; parse out the api call
+        (get-res (subs line (first (keys (re-pos #"\bget-\b" line))))
+          #"\bget-[A-Za-z]+-*[A-Za-z]*-*[A-Za-z]*\b")
+        ; parse out the time in ms
+        (Integer/parseInt
+          (get-res (subs line (first (keys (re-pos #"\bget-\b" line))))
+            #"\b[0-9]+\b")))
+      )))
+
+(defn merge-as-avg
+  " Merge function for api call duration maps."
+  [v1 val-in-latter]
+  (/ (+ v1 val-in-latter) 2.0))
+
+(defn get-all-api-times
+  "Returns a collection of maps of all found api-calls in an ASCII file. Where
+  name is the api call, value is the duration time."
+  [filename]
+  (map 
+    get-api-call-time
+    (duck/read-lines filename)))
+
+(defn merge-all-api-times
+  "Merges api-call time maps from each parsed file."
+  [log-dir]
+    (loop
+      [ret {} 
+       maps (filter not-empty (flatten (map get-all-api-times 
+              (walkr log-dir #".*\.access.log"))))]
+      (if-let [mp (first maps)]
+        (recur (merge-with merge-as-avg ret mp) (next maps))
+        ret)))
+
+; Generate a massive list of maps where key is the api call, val is time
+; Requires a merge func to do anything with 'em  
+(def maps (filter not-empty (flatten 
+            (map 
+              get-all-api-times
+              (walkr log-dir #".*\.access.log")))))
+
+; Merging down the above list of maps, calculating averages
+(def call-avgs (reduce #(merge-with merge-as-avg %1 %2) maps))
+; view 
+(view (pie-chart (keys call-avgs) (vals call-avgs)
+       :title "api call avgs (ms)"
+       :legend true))
 ;------------------------------------------------------------------------------
 ; get the api-key
 ;------------------------------------------------------------------------------
@@ -98,6 +150,7 @@
     (re-seq
       #"\b[A-Za-z0-9]{24}\b"
       in-str)))
+
 ;------------------------------------------------------------------------------
 ; IP address processing
 ;------------------------------------------------------------------------------
@@ -156,13 +209,13 @@
         :when (not (nil? k))] {k v})))
 
 ; if you want them sorted
+(def calls (get-all-calls log-dir))
 (def sorted-calls 
   (into 
     (sorted-map-by 
       (fn [key1 key2] (<= (calls key2) (calls key1))))
     calls))
 
-(def calls (get-all-calls log-dir))
 (view (pie-chart (keys calls) (vals calls)
        :title "api calls"
        :legend true))
@@ -196,8 +249,6 @@
 (view (bar-chart (keys ips2) (vals ips2)))
 ; get the top n ip addresses by hits
 (def max-hits 5)
-(def locations
-    (map get-location (take max-hits (keys ips2))))
 
 (view (bar-chart (take max-hits (keys ips2))
                   (take max-hits (vals ips2))
@@ -218,12 +269,6 @@
 ;------------------------------------------------------------------------------
 ; post to get the lat, lng of the ip...bit of a hack
 ;------------------------------------------------------------------------------
-(defn get-location
-  [ip-addr-str]
-  (get-google-lat-lng
-    (http/string (http/http-agent "http://www.ip-address.org/lookup/ip-locator.php" 
-                              :method "POST" 
-                              :body (str "ip=" ip-addr-str)))))
 (defn get-google-lat-lng
   "If it exists, returns the string lat, lng within the given string.
   Mega-hack because it is completely tailored to a specific response."
@@ -237,6 +282,14 @@
           ))
       #"\)")))
 
+(defn get-location
+  [ip-addr-str]
+  (get-google-lat-lng
+    (http/string (http/http-agent "http://www.ip-address.org/lookup/ip-locator.php" 
+                              :method "POST" 
+                              :body (str "ip=" ip-addr-str)))))
+(def locations
+    (map get-location (take max-hits (keys ips2))))
 ;------------------------------------------------------------------------------
 ; prototype code below...
 ;------------------------------------------------------------------------------
